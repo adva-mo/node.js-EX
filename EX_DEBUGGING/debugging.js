8250bf3caac339a4a50c6f44bb539f84725ebbc8
adva-mo <advamozes@gmail.com>
2022-12-05 17:40:59 +0200

    Folders organization

--- EX_DEBUGGING/debugging.js
+++
@@ -1,1 +0,0 @@
-//? the node debugger is using the built in v8 debugger tool, that means chrome is the only way on the browser to inspect our program
--- utils.js
+++
@@ -1,9 +0,0 @@
-const add = function (a, b) {
-  return a + b;
-};
-const minus = function (a, b) {
-  return a - b;
-};
-
-// exports.add = {add};
-module.exports = { add };
